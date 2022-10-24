100c5c8c8cc97d057233f8d396ee08d36b55f44c
LeonHuang-Utah <leonhuangwork@gmail.com>
2022-10-24 15:07:45 -0600

    pro sub 1

---
+++ publisher.js
@@ -0,0 +1,9 @@
+import { createClient } from 'redis';
+const publisher = createClient();
+
+async function main(){
+    await publisher.connect()
+    await publisher.publish('channel1', 'hello world', function(){process.exit(0)});
+}
+main()
+
---
+++ subscriber.js
@@ -0,0 +1,11 @@
+
+import { createClient } from 'redis';
+
+async function main(){
+    const subscriber = createClient()
+    await subscriber.connect()
+    subscriber.subscribe('channel1', (message) => {
+        console.log(message); // 'message'
+    });
+}
+main()
